1918df2abef2283401a255f4ac9afcfed4bd688d
Jan Kraus Stanko <krausjan@mediamarktsaturn.com>
2021-03-07 14:14:25 +0100

    Updates

--- _next/static/current/_ssgManifest.js
+++ _next/static/current/_ssgManifest.js
@@ -1,1 +1,1 @@
-self.__SSG_MANIFEST=new Set(["\u002Fmission","\u002F404","\u002Fprivacy","\u002Fdevelopers","\u002Fpress","\u002Fcollectives","\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set(["\u002F404","\u002Fmission","\u002Fprivacy","\u002Fcollectives","\u002Fdevelopers","\u002Fpress","\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
