9c7d0755303dbf7047ef1e1e99d524c1366b3c60
raykyri <raykyri@users.noreply.github.com>
2021-07-18 18:04:36 +0000

    Deploying to gh-pages from @ raykyri/Edgeware-Website@ec2262ad6e831c4fa20aa7806e970c1ca74f1d67 🚀

--- _next/static/current/_ssgManifest.js
+++ _next/static/current/_ssgManifest.js
@@ -1,1 +1,1 @@
-self.__SSG_MANIFEST=new Set(["\u002F404","\u002Fmission","\u002Fprivacy","\u002Fcollectives","\u002Fpress","\u002Fdevelopers","\u002F"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set(["\u002F404","\u002Fcollectives","\u002Fdevelopers","\u002F","\u002Fjobs","\u002Fkeygen","\u002Fmission","\u002Fpress","\u002Fprivacy","\u002Fjobs\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
